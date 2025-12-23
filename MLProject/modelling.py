64ac0a46b55b6c318822cc231a0a6bf359b6f0c5
Fadia Zahran Zain <fadiazzahranzain@gmail.com>
2025-12-24 03:09:59 +0700

    initial mlflow ci pipeline

---
+++ MLProject/modelling.py
@@ -0,0 +1,50 @@
+import mlflow
+import mlflow.xgboost
+import pandas as pd
+
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score
+
+# =========================
+# Load dataset
+# =========================
+data = pd.read_csv("diabetes_preprocessing/diabetes_preprocessed.csv")
+
+TARGET_COL = "diabetes"
+
+X = data.drop(TARGET_COL, axis=1)
+y = data[TARGET_COL]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+# =========================
+# Training
+# =========================
+with mlflow.start_run():
+
+    model = XGBClassifier(
+        n_estimators=200,
+        max_depth=6,
+        learning_rate=0.1,
+        random_state=42,
+        eval_metric="logloss"
+    )
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("f1_score", f1)
+
+    mlflow.xgboost.log_model(model, "model")
+    print(f"Model trained with Accuracy: {acc:.4f}, F1 Score: {f1:.4f}")
