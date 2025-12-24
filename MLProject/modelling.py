48fe6a079b31e7ac2e6763d1cecbb867bcff9239
Fadia Zahran Zain <fadiazzahranzain@gmail.com>
2025-12-24 15:27:40 +0700

    Disable model artifact logging for CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -51,7 +51,7 @@ def main(data_path):
         mlflow.log_metric("accuracy", acc)
         mlflow.log_metric("f1_score", f1)
 
-        mlflow.xgboost.log_model(model, "model")
+        # mlflow.xgboost.log_model(model, "model")
 
         print(f"Model trained with Accuracy: {acc:.4f}, F1 Score: {f1:.4f}")
 
