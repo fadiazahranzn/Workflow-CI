af240150337125c8fa0b81888073a46e28fe14ae
Fadia Zahran Zain <fadiazzahranzain@gmail.com>
2025-12-24 13:40:16 +0700

    Finalize MLflow Project for CI and Docker

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,3 +1,4 @@
+import argparse
 import mlflow
 import mlflow.xgboost
 import pandas as pd
@@ -6,45 +7,60 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
 
-# =========================
-# Load dataset
-# =========================
-data = pd.read_csv("diabetes_preprocessing/diabetes_preprocessed.csv")
 
-TARGET_COL = "diabetes"
+def main(data_path):
+    # =========================
+    # Load dataset
+    # =========================
+    data = pd.read_csv(data_path)
 
-X = data.drop(TARGET_COL, axis=1)
-y = data[TARGET_COL]
+    TARGET_COL = "diabetes"
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=0.2,
-    random_state=42,
-    stratify=y
-)
+    X = data.drop(TARGET_COL, axis=1)
+    y = data[TARGET_COL]
 
-# =========================
-# Training
-# =========================
-with mlflow.start_run():
-
-    model = XGBClassifier(
-        n_estimators=200,
-        max_depth=6,
-        learning_rate=0.1,
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.2,
         random_state=42,
-        eval_metric="logloss"
+        stratify=y
     )
 
-    model.fit(X_train, y_train)
+    # =========================
+    # Training
+    # =========================
+    with mlflow.start_run():
 
-    y_pred = model.predict(X_test)
+        model = XGBClassifier(
+            n_estimators=200,
+            max_depth=6,
+            learning_rate=0.1,
+            random_state=42,
+            eval_metric="logloss"
+        )
 
-    acc = accuracy_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
+        model.fit(X_train, y_train)
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.log_metric("f1_score", f1)
+        y_pred = model.predict(X_test)
 
-    mlflow.xgboost.log_model(model, "model")
-    print(f"Model trained with Accuracy: {acc:.4f}, F1 Score: {f1:.4f}")
+        acc = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+
+        mlflow.log_metric("accuracy", acc)
+        mlflow.log_metric("f1_score", f1)
+
+        mlflow.xgboost.log_model(model, "model")
+
+        print(f"Model trained with Accuracy: {acc:.4f}, F1 Score: {f1:.4f}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        default="diabetes_preprocessing/diabetes_preprocessed.csv"
+    )
+    args = parser.parse_args()
+
+    main(args.data_path)
